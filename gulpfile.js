0bd2fbc40a5e089b4d686dab71e54c78d3d3eee3
Kenneth Bambridge <kbambridge1@gmail.com>
2017-05-18 00:55:45 -0400

    Update gulpfile to remove deprecated `gulp.run`

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,14 @@ var gulp = require('gulp'),
  * description: launch the server. If there's a server already running, kill it.
  */
 gulp.task('server', function() {
-  if (node) node.kill();
-  node = spawn('node', ['app.js'], {stdio: 'inherit'});
-  node.on('close', function (code) {
-    if (code === 8) {
-      gulp.log('Error detected, waiting for changes...');
-    }
+  gulp.watch(['./app.js', './lib/**/*.js'], function() {
+      if (node) node.kill();
+      node = spawn('node', ['app.js'], {stdio: 'inherit'});
+      node.on('close', function (code) {
+        if (code === 8) {
+          gulp.log('Error detected, waiting for changes...');
+        }
+      });
   });
 });
 
@@ -20,17 +22,7 @@ gulp.task('server', function() {
  * $ gulp
  * description: start the development environment
  */
-gulp.task('default', function() {
-  gulp.run('server');
-
-  gulp.watch(['./app.js', './lib/**/*.js'], function() {
-    gulp.run('server');
-  });
-
-  // Need to watch for sass changes too? Just add another watch call!
-  // no more messing around with grunt-concurrent or the like. Gulp is
-  // async by default.
-});
+gulp.task('default', ['server']);
 
 // clean up if an error goes unhandled.
 process.on('exit', function() {
